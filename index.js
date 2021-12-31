91fd860ce0e4960f4751d92d25c1eea7e17ee656
pratik sonar <pratik.sonar@zingworks.in>
2021-12-31 17:26:49 +0530

    swagger-initialized

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const swaggerUi = require("swagger-ui-express")
 
 const PORT = process.env.PORT || 8000
 
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "my-api",
+            description: "my-api for learnings.",
+            contact: {
+                name: "pratik sonar"
+            },
+            servers: ["https://localhost:8000"]
+        }
+    },
+    //['.routes/*.js']
+    apis: ["index.js"]
+}
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+
+
 app.listen(PORT, () => {
-    console.log("server is working on" + PORT)
+    console.log("server is working on " + PORT)
 })
